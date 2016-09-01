a6fce5cb92202a8bc31514c5ec95cdb9269e7a4c
aldernetwork <aldernetworks@gmail.com>
2016-08-31 23:03:09 -0700

    [386. Lexicographical Numbers][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 386-Lexicographical-Numbers/solution.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    int digits(int n) {
+        int res = 0;
+        if (n==0)
+            return 1;
+            
+        while (n) {
+            n /= 10;
+            res++;
+        }
+        return res;
+    }
+    vector<int> lexicalOrder(int n) {
+        vector<int> res;
+        vector<int> stack;
+        res.push_back(1);
+        int dn = digits(n);
+        do {
+            int top = res.back(), to_bt = 0;
+            if (top < n)
+            {
+                int dtop = digits(top), ntop = top;
+                if (dtop < dn) 
+                {
+                    int save = top+1, save_digits=digits(save);
+                    if (save <= n && (save % 10) != 0)
+                    {
+                        stack.push_back(save);
+                    }
+                    ntop *= 10; 
+                    if (ntop <= n)
+                    {
+                        res.push_back(ntop);
+                    }
+                    else
+                    {
+                        to_bt = 1;  
+                    }
+                }
+                else
+                {
+                    int ttop = ntop, last, unit = 1;  
+                    //while (ttop) {
+                    if (ttop) {
+                        last = ttop % 10;
+                        if ( last < 9)
+                        {
+                            ntop += unit;
+                            //break;
+                            
+                        }
+                        unit *= 10;
+                        ttop /= 10;
+                    }
+                    res.push_back(ntop);
+                }
+                
+            }
+            else
+            {
+                to_bt = 1;
+            }
+            
+            if (to_bt) {
+                if (stack.empty())
+                {
+                    break;
+                }
+                else
+                {
+                    top = stack.back();
+                    stack.pop_back();
+                    res.push_back(top);
+                }
+            }
+        } while (1);
+        return res;
+    }
+};
