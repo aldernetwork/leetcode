7c8d1eac24b1e36120d1c66718eb923d2bee0424
aldernetwork <aldernetworks@gmail.com>
2016-09-02 09:13:06 -0700

    [70. Climbing Stairs][Accepted]committed by LeetCode Extension

--- 70-Climbing-Stairs/solution.cpp
+++ 70-Climbing-Stairs/solution.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n < 2 )
-            return 1;
-        return  climbStairs(n-1) + (climbStairs(n-2));
+        int *res = new int[n+1];
+        res[0] = res[1] = 1;
+        for(int i=2; i<=n; i++)
+            res[i] = res[i-1] + res[i-2];
+        return  res[n];
         
     }
 };
