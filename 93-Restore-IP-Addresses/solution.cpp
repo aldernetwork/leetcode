ff8ca832a35191eb892e3d99fe171357db9f7cec
aldernetwork <aldernetworks@gmail.com>
2016-09-17 21:46:36 -0700

    [93. Restore IP Addresses][Accepted]committed by LeetCode Extension

--- 93-Restore-IP-Addresses/solution.cpp
+++ 93-Restore-IP-Addresses/solution.cpp
@@ -4,12 +4,12 @@ public:
     vector<string> restoreIp(string s, int num) {
         vector<string> res, res1;
         
-        if (0 == num || s.empty())
+        if (0 >= num || s.empty())
             return res;
             
         if (1 == num || s.empty())
         {
-            if (s.empty() || atoi(s.c_str()) > 255) 
+            if (s.empty() || s.size() >= 4 || atoi(s.c_str()) > 255) 
             {
                 return res;
             }
@@ -23,12 +23,15 @@ public:
             
         }
         
-        for (int sz = 1; sz < 4; sz++)
+        int max = min(3, (int)(s.size()));
+        for (int sz = 1; sz <= max; sz++)
         {
             string ip = s.substr(0, sz);
-            if (sz == 3 && atoi(ip.c_str()) > 255 || ((sz>1) && (ip[0] == '0')))
+            if (sz == 3 && atoi(ip.c_str()) > 255 || ((sz>=2) && (ip[0] == '0')))
                 break;
-                
+            
+            if (num <= 1 || s.size() <= sz)
+                break;
             res1 = restoreIp(s.substr(sz, s.size()-sz), num-1);
  
             for (int i = 0; i < res1.size(); i++)  
