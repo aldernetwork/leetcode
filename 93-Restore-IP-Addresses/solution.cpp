51f1f4bc32ea1b091c741ae2f36eac195a984004
aldernetwork <aldernetworks@gmail.com>
2016-09-17 21:30:58 -0700

    [93. Restore IP Addresses][Runtime Error]committed by LeetCode Extension

--- 93-Restore-IP-Addresses/solution.cpp
+++ 93-Restore-IP-Addresses/solution.cpp
@@ -4,9 +4,14 @@ public:
     vector<string> restoreIp(string s, int num) {
         vector<string> res, res1;
         
-        if (1 == num)
+        if (1 >= num || s.empty())
         {
             if (s.empty() || atoi(s.c_str()) > 255) 
+            {
+                return res;
+            }
+            
+            if (s.size()>1 && s[0] == '0')
             {
                 return res;
             }
@@ -18,12 +23,10 @@ public:
         for (int sz = 1; sz < 4; sz++)
         {
             string ip = s.substr(0, sz);
-            if (sz == 3 && atoi(ip.c_str()) > 255)
+            if (sz == 3 && atoi(ip.c_str()) > 255 || ((sz>1) && (ip[0] == '0')))
                 break;
             res1 = restoreIp(s.substr(sz, s.size()-sz), num-1);
-            if (res1.empty())
-                break;
-           
+ 
             for (int i = 0; i < res1.size(); i++)  
             {
                 string ipStr = ip;
